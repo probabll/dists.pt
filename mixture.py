04156f8db96ee0d2da2aa35baad337c27e35de9e
Wilker Aziz <will.aziz@gmail.com>
2019-08-06 10:23:18 +0200

    another discrete/continuous mixture

--- mixture.py
+++ mixture.py
@@ -59,6 +59,60 @@ class MixtureD0C01(torch.distributions.Distribution):
         h = self.p0 * ( - self.log_p0) + self.pc * (- self.log_pc) + self.pc * self.cont.entropy()
         return h
 
+
+class MixtureD01C01(torch.distributions.Distribution):
+
+    def __init__(self, logits, cont, validate_args=None):
+        """
+
+        logits: [..., 3]
+        cont: a (properly normalised) distribution over (0, 1)
+            e.g. RightTruncatedExponential
+        """
+        shape = cont.batch_shape
+        super(MixtureD01C01, self).__init__(batch_shape=shape, validate_args=validate_args)
+        self.logits = logits
+        self.probs = F.softmax(logits, dim=-1)
+        self.logprobs = F.log_softmax(logits, dim=-1)
+        self.cont = cont
+        self.p0, self.p1, self.pc = [t.squeeze(-1) for t in torch.split(self.probs, 1, dim=-1)]
+        self.log_p0, self.log_p1, self.log_pc = [t.squeeze(-1) for t in torch.split(self.logprobs, 1, dim=-1)]
+        self.uniform = Uniform(torch.zeros(shape).to(logits.device),
+                               torch.ones(shape).to(logits.device))
+
+    def rsample(self, sample_shape=torch.Size()):
+        # sample from (0, 1) uniformly
+        u = self.uniform.rsample(sample_shape)
+        # affine transform to project from (p_0, 1) to (0, 1)
+        # note that only where p_0 < u < 1 this is correct
+        # print(u.size(), self.p0.size(), self.pc.size())
+        to_cont = (u - self.p0) / self.pc
+        # c ~ ContinuousDist()
+        # note where p_0 < u < 1, c is valid and is in (0,1)
+        x = self.cont.icdf(to_cont)
+        # inverse cdf of mixture model
+        # 0 if u < p_0
+        # c otherwise
+        x = torch.where(u <= self.p0, torch.zeros_like(u), x)
+        x = torch.where(u >= self.p0 + self.pc, torch.ones_like(u), x)
+        return x
+
+    def log_prob(self, value):
+        log_prob = self.log_pc + self.cont.log_prob(value)
+        log_prob = torch.where(value == 0., self.log_p0, log_prob)
+        log_prob = torch.where(value == 1., self.log_p1, log_prob)
+        return log_prob
+
+    def cdf(self, value):
+        cdf = self.p0 + self.pc * self.cont.cdf(value)
+        cdf = torch.where(value == 0., self.p0, cdf)
+        cdf = torch.where(value == 1., self.p1, cdf)
+        return cdf
+
+    def entropy(self):
+        h = - self.p0 * self.log_p0 - self.p1 * self.log_p1 - self.pc * self.log_pc + self.pc * self.cont.entropy()
+        return h
+
     
 def kl_mixture_mixture(p, q):   
     # see derivation on overleaf

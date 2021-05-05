07f698acf4e200dca84c0242c39aca6b04e8c7dd
Wilker Aziz <will.aziz@gmail.com>
2021-05-05 15:59:04 +0200

    NonEmptyBitVector and MaskedDirichlet

---
+++ probabll/distributions/bitvector.py
@@ -0,0 +1,383 @@
+import torch
+import torch.distributions as td
+from torch.autograd import Function
+from torch.autograd.function import once_differentiable
+from torch.distributions import constraints
+from torch.distributions.exp_family import ExponentialFamily
+from itertools import product
+import numpy as np
+
+
+class NonEmptyBitVector(td.Distribution):
+    """
+    This class manipulates distributions over bit-vectors with the constraint that the outcome 'all zeros' is not in the sample space.
+    """
+
+    has_rsample = False
+    has_enumerate_support = True
+
+    @classmethod
+    def _arc_weight_and_state_value(cls, scores):
+        """
+        For a batch of size B, we build an FSA to sample from a distribution
+        over {0,1}^K \setminus {0^K}
+
+        Each distribution assigns probability 
+        p(x|scores) \propto \prod_k \exp(x_k * scores_k - (1-x_k) * scores_k)
+
+        We need to normalise the distribution over valid assignments of x, this 
+        affects the pmf and thus also sampling. For that we build an FSA with 
+        (K+1)*3 states. Each state is identified by an index k 
+        (a position in a K-dimensional bit vectore)
+        and a label L (out of 3 possible labels):
+
+        * a state (k, L=0) means that the kth bit is 0 and sum(x[:k+1])=0
+        * a state (k, L=1) means that the kth bit is 0 and thus sum(x[:k+1])>0
+        * a state (k, L=2) means that the kth bit is 1 and thus sum(x[:k+1])>0
+
+        The initial state of the FSA is (k=0, L=0) 
+        and it connects to (k=1,L=0) or (k=1, L=2), 
+        each arc weighted with the corresponding log potential.
+        The final state is (k=K+1,L=1).
+
+        :param scores: log pontetials with shape [B, K]
+        :return:
+            * weight of outgoing arcs with shape [B, K+1, 3, 3]
+            * (backward/inside) value of states with shape [B, K+2, 3]
+        """
+        batch_shape, K = scores.shape[:-1], scores.shape[-1]
+        # initialise weights of outgoing arcs from initial state (k=0, L=*) 
+        #  and every intermediate state until the pre-final states (k=K, L=*)
+        # the initial value is semiring.zero (-inf for LogProb semiring)
+        W = torch.zeros(batch_shape + (K+1, 3, 3), device=scores.device) - np.inf
+        # we have 3 possible labels
+        # W[b, k, 0]: x_{b,k+1} is False, sum(x_{b,:k+1}) + x_{b,k+1} = 0
+        # W[b, k, 1]: x_{b,k+1} is False, sum(x_{b,:k+1}) + x_{b,k+1} > 0
+        # W[b, k, 2]: x_{b,k+1} is True, sum(x_{b,:k+1}) + x_{b,k+1} > 0
+
+        # Our scores are log potentials (and semiring.zero = -inf)
+        ninf = torch.zeros_like(scores) - np.inf
+
+        # For states corresponding to k <= K we have
+
+        # arcs from (k,L=0) to (k+1,L=0) and (k+1,L=2) 
+        W[...,:-1,0,:] = torch.stack([-scores, ninf, scores], -1)
+        # arcs from (k,L=1) to (k+1,L=1) and (k+1,L=2) 
+        W[...,1:-1,1,:] = torch.stack([ninf[...,1:], -scores[...,1:], scores[...,1:]], -1)
+        # arcs from (k,L=2) to (k+1,L=1) and (k+1,L=2)
+        W[...,1:-1,2,:] = torch.stack([ninf[...,1:], -scores[...,1:], scores[...,1:]], -1)
+
+        # arcs from pre-final states to final state
+        W[...,K,1,1] = 0
+        W[...,K,2,1] = 0
+
+        # Compute values and reverse values: https://www.aclweb.org/anthology/J99-4004.pdf
+        # This can be a bit confusing, so let me make some connections.
+        # The value recursion is known as the backward algorithm for HMMs
+        #  or inside algorithm for PCFGs.
+        # The reverse value recursion is known as the forward algorithm for HMMs
+        #  (which is just the backward ran the other way around)
+        #  or inside algorithm for PCFGs (unlike in the case of forward/backward, 
+        #  inside is not just outside in a "reversed" graph).
+        # I am using the language of value/reverse value simply because of familiarity
+        #  and because these concepts are less confusing to me.
+
+        # Initialise the value of every state with semiring.zero
+        V = torch.zeros(batch_shape + (K+2, 3), device=scores.device) - np.inf
+        # the only final state (k=K+1, L=1), has weight semiring.one
+        V[...,K+1,1] = 0
+        # The value of the initial state will give us the log normaliser of the distribution.
+
+        # Initialise the reverse value of every state with semiring.zero
+        R = torch.zeros(batch_shape + (K+2, 3), device=scores.device) - np.inf
+        # the only initial state (k=0,L=0) has weight semiring.one
+        R[...,0,0] = 0
+
+        # Compute values and reverse values in a single linear pass
+        for k in torch.flip(torch.arange(K+1, device=scores.device), [-1]): 
+            V[...,k,:] = torch.logsumexp(W[...,k,:,:] + V[...,k+1,None,:], dim=-1)
+            R[...,K-k+1,:] = torch.logsumexp(W[...,K-k,:,:] + R[...,K-k,:,None], dim=-2)        
+
+        return W, V, R
+    
+    def __init__(self, scores, validate_args=False):
+        """
+        :param scores: [B, K]
+        """
+        self._scores = scores  # [B, K]
+        batch_shape, K = scores.shape[:-1], scores.shape[-1]
+        if len(batch_shape) > 1:
+            raise ValueError("This implementation cannot take batch_shape more complex than [num]")
+        event_shape = torch.Size([])        
+        super(NonEmptyBitVector, self).__init__(batch_shape, event_shape, validate_args)
+
+        self._K = K
+        self._arc_weight, self._state_value, self._state_rvalue = self._arc_weight_and_state_value(scores)
+
+    def expand(self, batch_shape, _instance=None):
+        new = self._get_checked_instance(NonEmptyBitVector, _instance)
+        batch_shape = torch.Size(batch_shape)
+        new._scores = self._scores.expand(batch_shape + self.event_shape)
+        new._arc_weight = self._arc_weight.expand(batch_shape + self.event_shape)
+        new._state_value = self._state_value.expand(batch_shape + self.event_shape)
+        new._state_rvalue = self._state_rvalue.expand(batch_shape + self.event_shape)
+        super(NonEmptyBitVector, new).__init__(batch_shape, self.event_shape, validate_args=False)
+        new._validate_args = self._validate_args
+        return new
+
+    @property
+    def dim(self):
+        return self._K 
+
+    @property
+    def support_size(self):
+        return 2 ** self._K - 1
+
+    @property
+    def scores(self):
+        return self._scores
+
+    @property
+    def arc_weight(self):
+        return self._arc_weight
+
+    @property
+    def state_value(self):
+        return self._state_value
+
+    @property
+    def state_rvalue(self):
+        return self._state_rvalue
+    
+    def log_prob(self, value):        
+        log_prob = torch.where(value.bool(), self._scores, -self._scores).sum(-1)
+        return log_prob - self._state_value[...,0,0]
+
+    def sample(self, sample_shape=torch.Size()):    
+        if len(sample_shape) > 1:
+            raise ValueError("This implementation cannot take sample_shape more complex than [num]")
+
+        with torch.no_grad():
+            batch_shape, K = self.batch_shape, self._K
+            # This will store the sequence of labels from k=0 to K+1
+            # [sample_shape, B, K+2]
+            S = torch.zeros(sample_shape + batch_shape + (K+2,), device=self._scores.device).long()            
+            # [sample_shape, B, K+2, 3]
+            eps = td.Gumbel(loc=torch.zeros(S.shape + (3,), device=self._scores.device), scale=1).sample()
+            # [...,K+1,3,3]
+            W = self._arc_weight
+            # [...,K+2,3]
+            V = self._state_value
+            for k in torch.arange(K+1, device=self._scores.device): 
+                if len(batch_shape) == 0:
+                    # [..., 3]
+                    logits_k = W[...,k,S[...,k],:] + V[...,k+1,:]
+                else:
+                    # [...,3,3]
+                    W_k = W[...,k,:,:]
+                    # [...,3]
+                    v_kp1 = V[...,k+1,:] 
+                    idx = S[...,k]  
+                    # batched selection 
+                    # torch.arange selects the batch, idx selects the step
+                    # [...,3]
+                    logits_k = W_k[torch.arange(len(W_k), device=W_k.device).unsqueeze(-1), idx.transpose(1,0)].transpose(1,0) + v_kp1                
+                # Categorical sampling
+                #S[...,k+1] = td.Categorical(logits=logits_k).sample().long()
+                # Categorical sampling via Gumbel-Argmax
+                S[...,k+1] = torch.argmax(logits_k + eps[...,k+1,:], -1).long()                
+
+            assert (S[...,-1] == 1).all(), "Not every sample reached the final state"
+            S = S[...,1:-1]  # discard the initial (k=0) and final (k=K+1) states
+            # map to boolean and then float (in torch discrete samples are float)
+            return (S == 2).float()
+
+    def enumerate_support(self, expand=True, max_K=10):
+        """
+        Return a bit-vector representation of all non-empty faces of 
+        K-1 dimensional simplex. 
+
+        The return has dtype=float (rather than bool) because in torch 
+        outcomes are always float. The shape is [2^K-1, B, K]
+        """
+        if max_K:
+            assert self._K <= max_K, f"You probably do not want to enumerate the {self.support_size} outcomes in the sample space?"
+        # [support_size, K]
+        faces = torch.tensor(
+            [x for x in product([0, 1], repeat=self._K) if sum(x)], 
+            device=self._scores.device).float()    
+        if expand:
+            num_faces, K = faces.shape
+            # [num_faces, ..., K]
+            faces = faces.view((num_faces,) + (1,)*len(self.batch_shape) + (K,))
+            # [num_faces, B, K]
+            faces = faces.expand((num_faces,)  + self.batch_shape + (K,))            
+        return faces 
+
+    def cross_entropy(self, other):
+        """
+        Compute H(p, q) = - \sum_x p(x) \log q(x)
+         where p = self, and q = other.
+
+        :return: [B]        
+        """
+        assert self.scores.shape == other.scores.shape, "The shape of the scores must match"
+        
+        # We have two global models:
+        #   p(x) = (\prod_{e in x} exp(score_p(e))) / Z_p        
+        # where Z_p is the normalisation constant
+        #   Z_p = \sum_x \prod_{e in x} exp(score_p(e))        
+        # and similarly
+        #   q(x) = (\prod_{e in x} exp(score_q(e))) / Z_q
+        #   Z_q = \sum_x \prod_{e in x} exp(score_q(e))        
+        # thus 
+        #   log p(x) = (\sum_{e in x} score_p(e)) - log Z_p
+        #   log q(x) = (\sum_{e in x} score_q(e)) - log Z_q
+
+        # The cross-entropy is
+        #  E[-log q(x)] = -\sum_x p(x) log q(x)
+        #   = \sum_x p(x) (log Z_q - \sum_{e in x} score_q(e))
+        #   = \sum_x p(x) log Z_q - \sum_x p(x) \sum_{e in x} score_q(e) 
+        #   = log Z_q - \sum_x p(x) \sum_{e in x} score_q(e)
+        #   = log Z_q - \sum_{e} mu(e) score_q(e)
+        # where mu(e) is the marginal probability of the edge under the distribution p_X
+        #  mu(e) = R[ori(e)] * exp(score_p(e)) * V[dest(e)]  / Z_p
+        #  R[ori(e)] is the total probability of all paths from the initial state
+        #  of the FSA to the origin state of the edge
+        #  V[dest(e)] is the total probability of all paths from the final state 
+        #  of the FSA to the destination state of the edge.
+
+        # A fully vectorised implementation is possible, but it's tricky to 
+        #  visualise. I'll try my best to explain it here.
+        W = self._arc_weight
+        V = self._state_value
+        R = self._state_rvalue
+        # Recall that
+        #  * W.shape is [B, K+1, 3, 3]
+        #  * V.shape is [B, K+2, 3]        
+        #  and the batch dimension B could actually be a tuple of dimensions.
+        # I want to vectorise the following computation
+        #  R[...,k,ori] + W[...,k,ori,dest] + V[...,k+1,dest]) - V[...,0,0]
+        #  for some k in {0,...,K}, ori in {0,1,2} and dest in {0,1,2}, here
+        #  ori is the label of the origin state, and dest is the label of the 
+        #  destination state.
+        # So I am going to change R's shape, 
+        #  it goes from [B, K+2, 3] to [B, K+1, 3, 1],
+        #  where I forget k=K+1 and unsqueeze the last dimension.
+        # Similarly, V goes from [B, K+2, 3] to [B, K+1, 1, 3],
+        #  where I forget k=0 and unsqueeze the second last dimension.
+        # The result has the same shape of W, which is desirable
+        #  since we are computing expected values for the edge potentials
+        # [B, K+1, 3, 3]
+        log_mu = R[...,:-1,:,None] + W + V[...,1:,None,:] - V[...,0,0,None,None,None]
+        # We need the marginal (not its log)
+        mu = log_mu.exp()
+        # Here we use masked product, this is needed because we want the semantics
+        # inf * 0 = 0, but for good reasons that's not what torch produces 
+        # in this specific context, a masked product is wanted and safe.
+        expected = torch.where(mu == 0, torch.zeros_like(mu), other._arc_weight * mu)
+        # For the entropy simply compute the expected potential and shift by log Z_q
+        H = -(expected.sum((-1, -2, -3)) - other._state_value[...,0,0])
+        
+        # less vectorised code (easier to read)
+        #nH = 0.
+        #for k in range(K+1):
+        #    for ori in range(3):            
+        #        for dest in range(3):
+        #            # marginal probability of edge
+        #            log_w = R[...,k,ori] + W[...,k,ori,dest] + V[...,k+1,dest] - V[...,0,0]
+        #            w = log_w.exp()                                        
+        #            # expected score
+        #            e = other._arc_weight[...,k,ori,dest] * w
+        #            e = torch.where(w == 0, torch.zeros_like(e), e)
+        #            nH = nH + e
+        #H = - (nH - other._state_value[...,0,0])
+
+        return H
+
+    def entropy(self):
+        return self.cross_entropy(self)
+
+    def mode(self):
+        """
+        Return the outcome x whose p_X(x) is maximum. 
+        This implementation always returns a single outcome (even if there are ties).
+        """
+        with torch.no_grad():
+            batch_shape, K = self.batch_shape, self._K
+            # This will store the sequence of labels from k=0 to K+1
+            S = torch.zeros(batch_shape + (K+2,), device=self._scores.device).long()
+            for k in torch.arange(K+1, device=self._scores.device): 
+                logits_k = self._arc_weight[...,k,S[...,k],:] + self._state_value[...,k+1,:]
+                # Categorical sampling via Gumbel-Argmax
+                S[...,k+1] = torch.argmax(logits_k, -1).long()
+
+            assert (S[...,-1] == 1).all(), "Not every sample reached the final state"
+            S = S[...,1:-1]  # discard the initial (k=0) and final (k=K+1) states
+            # map to boolean and then float (in torch discrete samples are float)
+            return (S == 2).float()
+
+@td.register_kl(NonEmptyBitVector, NonEmptyBitVector)
+def _kl_nonemptybitvector_nonemptybitvector(p, q):
+    if p.scores.shape != q.scores.shape: 
+        raise ValueError("The shapes of p and q differ")
+    return p.cross_entropy(q) - p.entropy()
+
+
+### Test stuff
+
+
+from collections import Counter 
+
+def test_non_empty_bit_vector(batch_shape=tuple(), K=3):
+    assert K<= 10, "I test against explicit enumeration, K>10 might be too slow for that"
+    
+    # Uniform 
+    F = NonEmptyBitVector(torch.zeros(batch_shape + (K,)))
+    
+    # Shapes
+    assert F.batch_shape == batch_shape, "NonEmptyBitVector has the wrong batch_shape"
+    assert F.dim == K, "NonEmptyBitVector has the wrong dim"
+    assert F.event_shape == tuple(), "NonEmptyBitVector has the wrong event_shape"
+    assert F.scores.shape == batch_shape + (K,), "NonEmptyBitVector.score has the wrong shape"
+    assert F.arc_weight.shape == batch_shape + (K+1,3,3), "NonEmptyBitVector.arc_weight has the wrong shape"
+    assert F.state_value.shape == batch_shape + (K+2,3), "NonEmptyBitVector.state_value has the wrong shape"
+    assert F.state_rvalue.shape == batch_shape + (K+2,3), "NonEmptyBitVector.state_rvalue has the wrong shape"
+    # shape: [num_faces] + batch_shape + [K]
+    support = F.enumerate_support()    
+    # test shape of support
+    assert support.shape == (2**K-1,) + batch_shape + (K,), "The support has the wrong shape"
+
+    # Constraints
+    assert (support.sum(-1) > 0).all(), "The support has an empty bit vector"
+    
+    # Distribution
+    # check for uniform probabilities
+    assert torch.isclose(F.log_prob(support).exp(), torch.tensor(1./F.support_size)).all(), "Non-uniform"
+    # check for uniform marginal probabilities
+    assert torch.isclose(F.sample((10000,)).float().mean(0), support.mean(0), atol=1e-1).all(), "Bad marginals"    
+
+    # Entropy
+
+    # [num_faces, B]
+    log_prob = F.log_prob(support)
+    assert torch.isclose(F.entropy(), (-(log_prob.exp() * log_prob).sum(0)), atol=1e-2).all(), "Problem in the entropy DP"
+
+    # Non-Uniform  
+
+    # Entropy  
+    P = NonEmptyBitVector(td.Normal(torch.zeros(batch_shape + (K,)), torch.ones(batch_shape + (K,))).sample())
+    log_p = P.log_prob(support)
+    assert torch.isclose(P.entropy(), (-(log_p.exp() * log_p).sum(0)), atol=1e-2).all(), "Problem in the entropy DP"
+    # Cross-Entropy
+    Q = NonEmptyBitVector(td.Normal(torch.zeros(batch_shape + (K,)), torch.ones(batch_shape + (K,))).sample())
+    log_q = Q.log_prob(support)
+    assert torch.isclose(P.cross_entropy(Q), -(log_p.exp() * log_q).sum(0), atol=1e-2).all(), "Problem in the cross-entropy DP"
+    # KL
+    assert torch.isclose(td.kl_divergence(P, Q), (log_p.exp() * (log_p - log_q)).sum(0), atol=1e-2).all(), "Problem in KL"
+
+test_non_empty_bit_vector(K=3)
+test_non_empty_bit_vector((3,), K=3)
+test_non_empty_bit_vector(K=10)
+test_non_empty_bit_vector((5,), K=10)
+
---
+++ probabll/distributions/dirichlet.py
@@ -0,0 +1,197 @@
+import torch
+import torch.distributions as td
+from torch.autograd import Function
+from torch.autograd.function import once_differentiable
+from torch.distributions import constraints
+from torch.distributions.exp_family import ExponentialFamily
+from itertools import product
+import numpy as np
+
+
+class MaskedDirichlet(td.Distribution):
+    """
+    This class allows us to batch Dirichlet distributions of varying dimensionality.
+    """
+
+    arg_constraints = {'concentration': td.constraints.independent(td.constraints.greater_than_eq(0.), 1)}
+    support = td.constraints.simplex
+    has_rsample = True
+
+    def __init__(self, mask, concentration, validate_args=None):
+        assert mask.shape == concentration.shape 
+
+        if validate_args:
+            if not (mask.sum(-1) > 0).all():
+                raise ValueError("A face must contain at least one vertix (check that every row in 'mask' contains at least one coordinate set to True)")    
+            if not (torch.where(mask, concentration, torch.ones_like(concentration)) > 0).all():
+                raise ValueError("The concentration parameter must be strictly positive for positions where mask is True")
+        
+        self._mask = mask
+        self._concentration = concentration
+
+        batch_shape, event_shape = concentration.shape[:-1], concentration.shape[-1:]
+        super(MaskedDirichlet, self).__init__(
+            batch_shape, 
+            event_shape, 
+            validate_args=validate_args)
+
+    def expand(self, batch_shape, _instance=None):
+        new = self._get_checked_instance(MaskedDirichlet, _instance)
+        batch_shape = torch.Size(batch_shape)
+        new._concentration = self._concentration.expand(batch_shape + self.event_shape)
+        new._mask = self._mask.expand(batch_shape + self.event_shape)
+        super(MaskedDirichlet, new).__init__(batch_shape, self.event_shape, validate_args=False)
+        new._validate_args = self._validate_args
+        return new
+
+    def rsample(self, sample_shape=torch.Size()):
+        # X_k | f ~ Gamma(alpha_{f,k}, 1)
+        X = td.Gamma(self._concentration, torch.ones_like(self._concentration), validate_args=False)  
+        # [batch_size, K]
+        x = X.rsample(sample_shape)
+        # now we mask the Gamma samples from invalid coordinates of lower-dimensional faces
+        x = torch.where(self._mask, x, torch.zeros_like(x))  
+        # finally, we renormalise the gamma samples
+        z = x / x.sum(-1, keepdim=True)
+        return z
+
+    def log_prob(self, value):
+        if self._validate_args:
+            self._validate_sample(value)        
+
+        # logarithm of Dirichlet normaliser (log Beta(alpha))       
+        zeros = torch.zeros_like(self._concentration)
+        log_beta_num = torch.where(self._mask, torch.lgamma(self._concentration), zeros).sum(-1)
+        log_beta_den = torch.lgamma(torch.where(self._mask, self._concentration, zeros).sum(-1))
+        log_beta = log_beta_num - log_beta_den
+
+        # logarithm of unnormalised density
+        log_prob = torch.where(self._mask, (self._concentration - 1) * torch.log(value), torch.zeros_like(value)).sum(-1)
+
+        # normalisation
+        log_prob = log_prob - log_beta
+        return log_prob
+
+    @property
+    def concentration(self):
+        """Masked concentration parameters"""
+        return torch.where(self._mask, self._concentration, torch.zeros_like(self._concentration))
+
+    @property
+    def mask(self):
+        return self._mask
+
+    @property
+    def dim(self):
+        """Dimensionality of each face as a tensor of dtype=int64 and shape=batch_shape"""
+        return self._mask.sum(-1)
+
+    @property
+    def mean(self):        
+        return self.concentration / self.concentration.sum(-1, True)
+
+    @property
+    def variance(self):
+        concentration = self.concentration
+        con0 = concentration.sum(-1, True)
+        return concentration * (con0 - concentration) / (con0.pow(2) * (con0 + 1))
+
+    def entropy(self):
+        # dimensionality of each Dirichlet
+        K = self._mask.float().sum(-1)
+        # masked concentrations
+        concentration = self.concentration
+        a0 = concentration.sum(-1)
+        zeros = torch.zeros_like(self._concentration)
+        H = torch.where(self._mask, torch.lgamma(self._concentration), zeros).sum(-1) 
+        H = H - torch.lgamma(a0) 
+        H = H - (K - a0) * torch.digamma(a0)
+        H = H - (torch.where(self._mask, (self._concentration - 1.0) * torch.digamma(self._concentration), zeros).sum(-1))
+        return H
+
+@td.register_kl(MaskedDirichlet, MaskedDirichlet)
+def _kl_maskeddirichlet_maskeddirichlet(p, q):
+    if p.mask.shape != q.mask.shape: 
+        raise ValueError("The shapes of p and q differ")
+    if not (p.mask == q.mask).all():
+        raise ValueError("The faces in p and q differ")
+    
+    p_concentration = p.concentration
+    q_concentration = q.concentration
+
+    sum_p_concentration = p_concentration.sum(-1)
+    sum_q_concentration = q_concentration.sum(-1)
+    t1 = sum_p_concentration.lgamma() - sum_q_concentration.lgamma()
+    
+    zeros = torch.zeros_like(p_concentration)
+    t2 = torch.where(p.mask, p_concentration.lgamma() - q_concentration.lgamma(), zeros).sum(-1)
+    t3 = p_concentration - q_concentration
+    t4 = torch.where(p.mask, p_concentration.digamma() - sum_p_concentration.digamma().unsqueeze(-1), zeros)
+    return t1 - t2 + (t3 * t4).sum(-1)
+
+
+### Test stuff
+
+def make_faces(K, dtype=bool, device=torch.device('cpu')):
+    """
+    Return a bit-vector representation of all non-empty faces of 
+    K-1 dimensional simplex. 
+
+    The return type is an np.array with dtype=bool and shape=[2^K-1, K]
+    """
+    faces = [x for x in product([0, 1], repeat=K) if sum(x)]
+    return torch.tensor(np.array(faces, dtype=dtype), device=device)
+
+
+def get_parameters(faces, dir_alpha=1, gamma_alpha=1, gamma_beta=1, device=torch.device('cpu')):
+    """
+    Return tf trainable parameters for a stratified distribution defined over the given faces
+        * mixing coefficients (omega) with shape [num_faces]
+        * Gamma concentrations (alpha) with shape [num_faces, K]
+
+    :param faces: bit-vector encoding of faces [num_faces, K]
+    :param dir_alpha: Dir concentration (float) used to sample the initial value of omega
+        set it to None to get omega_k = 1/K
+        set it to 'face_dim' to set each concentration parameter to the dimensionality of the face
+        this will bias the initial omega to be higher for higher-dimensional faces
+    :param gamma_alpha: Gamma shape parameter used to sample the initial values of alpha
+    :param gamma_beta: Gamma rate parameter used to sample the initial values of alpha
+    """
+    if dir_alpha is None:  # uniform mixing coefficients
+        omega = torch.tensor(np.ones(faces.shape[0])/faces.shape[0], requires_grad=True, device=device)
+    elif isinstance(dir_alpha, str) and dir_alpha == 'face_dim':
+        omega = torch.tensor(np.random.dirichlet(faces.float().sum(-1)), requires_grad=True, device=device)
+    elif isinstance(dir_alpha, float) or isinstance(dir_alpha, int):
+        omega = torch.tensor(np.random.dirichlet(np.ones(faces.shape[0]) * dir_alpha), requires_grad=True, device=device)
+    else:
+        raise ValueError("Use None, 'face_dim', or a strictly positive real for dir_alpha")
+    alphas = torch.tensor(np.random.gamma(gamma_alpha, 1/gamma_beta, size=faces.shape), requires_grad=True, device=device)
+    return omega, alphas
+
+def test_masked_dirichlet(K=3):
+    mask = make_faces(K)
+    w, con = get_parameters(mask, dir_alpha=None, gamma_alpha=1, gamma_beta=1)
+    p = MaskedDirichlet(mask, con)
+    q = MaskedDirichlet(mask, torch.ones_like(con))
+    assert (torch.where(torch.logical_not(mask), p.concentration, torch.zeros_like(con)) == 0).all(), "Masked concentration parameters should be 0.0"
+    assert (torch.where(mask, p.concentration, torch.ones_like(con)) > 0).all(), "Unmasked concentration parameters should be strictly positive"
+    for i, face in enumerate(mask):
+        idx = tuple(k for k, b in enumerate(face) if b)
+        alphas = con[i,idx]
+        p_low = td.Dirichlet(alphas)
+        q_low = td.Dirichlet(torch.ones_like(alphas))
+        assert torch.isclose(p.mean[i,idx], p_low.mean).all(), f"The {i}th face's mean does not match that of td.Dirichlet"
+        assert torch.isclose(p.variance[i,idx], p_low.variance).all(), f"The {i}th face's variance does not match that of td.Dirichlet"
+        assert torch.isclose(p.entropy()[i], p_low.entropy()).all(), f"The {i}th face's entropy does not match that of td.Dirichlet"
+        assert (p.dim[i] == len(idx)), f"The dimensionality of the {i}th face is incorrect: got {p.dim[i]}, expected {len(idx)}"
+        x = p.rsample()
+        assert torch.isclose(p.log_prob(x)[i], p_low.log_prob(x[i,idx])).all(), "The log_prob of a sample does not match that assigned by td.Dirichlet"
+        assert torch.isclose(td.kl_divergence(p, q)[i], td.kl_divergence(p_low, q_low)).all(), "The KL divergence does not match that of td.Dirichlet"
+        assert torch.isclose(td.kl_divergence(q, p)[i], td.kl_divergence(q_low, p_low)).all(), "The KL divergence does not match that of td.Dirichlet"
+
+
+test_masked_dirichlet(3)
+test_masked_dirichlet(5)
+test_masked_dirichlet(7)
+
+
